aa13762d88a5e49c08525956b15631a9d1dea84a
Ju <Ju@DESKTOP-78AT35D>
2020-10-25 23:39:55 -0300

    Commit criação da classe MarmitaDao e o procedimento addMarmita

---
+++ src/java/dao/MarmitaDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Marmita;
+import util.DbUtil;
+
+public class MarmitaDao {
+    
+    private final Connection connection;
+    
+    public MarmitaDao(){
+        connection = DbUtil.getConnection();
+    }
+    
+    public void addMarmita (Marmita marmita){
+        System.out.println("Entrei na addMarmita!");
+        try {
+            String SQL = "INSERT INTO tb_marmita(cd_nr_marmita, cd_grupoMarmita, ds_ingredientes, cd_tamanho, preco, nomeMarmita,foto, st_cardapio) VALUES"
+                    +"(?,?,?,?,?,?,?,?)";
+            try (PreparedStatement ps = connection.prepareStatement(SQL)){
+                ps.setInt(1, marmita.getNumeroMarmita());
+                ps.setString(2, marmita.getGrupoMarmita());
+                ps.setString(3, marmita.getIngredientes());
+                ps.setString(4, marmita.getTamanho());
+                ps.setShort(5,marmita.getPreco());
+                ps.setString(6, marmita.getNomeProduto());
+                ps.setBlob(7, marmita.getFoto());
+                ps.setBoolean(8, marmita.isStatusCardapio());
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
